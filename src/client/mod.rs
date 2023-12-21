2cdfb91d162fc9777eb6a3cbe152507dc721b237
Joxit <joxit972@gmail.com>
2023-12-21 02:11:29 +0100

    feat: add support to custom TLS and H2 in try_form

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -119,7 +119,9 @@ impl TryFrom<&str> for ClientType {
   type Error = anyhow::Error;
 
   fn try_from(s: &str) -> Result<ClientType, Self::Error> {
-    let regex = Regex::new(r"^(?<ip>\d+.\d+.\d+.\d+)(:(?<port>\d+)?)?$").unwrap();
+    let regex =
+      Regex::new(r"^(?<ip>\d+.\d+.\d+.\d+)(:(?<port>\d+)?:?((?<proto>h2|tls):(?<domain>.*))?)?$")
+        .unwrap();
     let Some(caps) = regex.captures(s) else {
       bail!("The option is not recognized");
     };
@@ -138,7 +140,28 @@ impl TryFrom<&str> for ClientType {
       }
     })?;
 
-    Ok(ClientType::CustomDNS(ip, port.unwrap_or(53)))
+    let proto = caps
+      .name("proto")
+      .map_or(None, |proto| Some(proto.as_str()));
+
+    let domain = caps
+      .name("domain")
+      .map_or(None, |domain| Some(domain.as_str().to_string()));
+
+    match proto {
+      Some("tls") => Ok(ClientType::CustomTLS(
+        ip,
+        domain.ok_or_else(|| anyhow!("No domain found for TLS connection."))?,
+        port.unwrap_or(853),
+      )),
+      Some("h2") => Ok(ClientType::CustomH2(
+        ip,
+        domain.ok_or_else(|| anyhow!("No domain found for TLS connection."))?,
+        port.unwrap_or(443),
+      )),
+      None => Ok(ClientType::CustomDNS(ip, port.unwrap_or(53))),
+      _ => bail!("The protocol {} is not supported", proto.unwrap()),
+    }
   }
 }
 
@@ -169,5 +192,57 @@ mod test {
     assert!(ClientType::try_from("example.com:53").is_err());
     assert!(ClientType::try_from("example.com").is_err());
     assert!(ClientType::try_from("256.255.254.253").is_err());
+  }
+
+  #[test]
+  pub fn covert_custom_tls() {
+    let cloudflare = "cloudflare-dns.com";
+    let ip = ClientType::try_from("1.1.1.1:tls:cloudflare-dns.com");
+    let ip_port = ClientType::try_from("1.1.1.1:1853:tls:cloudflare-dns.com");
+
+    assert!(ip.is_ok());
+    assert!(ip_port.is_ok());
+
+    assert_eq!(
+      ip.unwrap(),
+      ClientType::CustomTLS(ipv4("1.1.1.1"), cloudflare.to_string(), 853)
+    );
+    assert_eq!(
+      ip_port.unwrap(),
+      ClientType::CustomTLS(ipv4("1.1.1.1"), cloudflare.to_string(), 1853)
+    );
+
+    assert!(ClientType::try_from("1.1.1.1:853:tls").is_err());
+    assert!(ClientType::try_from("1.1.1.1:-853:tls:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("1.1.1.1:0:tls:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("example.com:853:tls:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("example.com:tls:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("256.255.254.253:tls:cloudflare-dns.com").is_err());
+  }
+
+  #[test]
+  pub fn covert_custom_h2() {
+    let cloudflare = "cloudflare-dns.com";
+    let ip = ClientType::try_from("1.1.1.1:h2:cloudflare-dns.com");
+    let ip_port = ClientType::try_from("1.1.1.1:1443:h2:cloudflare-dns.com");
+
+    assert!(ip.is_ok());
+    assert!(ip_port.is_ok());
+
+    assert_eq!(
+      ip.unwrap(),
+      ClientType::CustomH2(ipv4("1.1.1.1"), cloudflare.to_string(), 443)
+    );
+    assert_eq!(
+      ip_port.unwrap(),
+      ClientType::CustomH2(ipv4("1.1.1.1"), cloudflare.to_string(), 1443)
+    );
+
+    assert!(ClientType::try_from("1.1.1.1:443:h2").is_err());
+    assert!(ClientType::try_from("1.1.1.1:-443:h2:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("1.1.1.1:0:h2:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("example.com:443:h2:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("example.com:h2:cloudflare-dns.com").is_err());
+    assert!(ClientType::try_from("256.255.254.253:h2:cloudflare-dns.com").is_err());
   }
 }
